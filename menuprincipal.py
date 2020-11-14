0d0da8284d5ce020b22b09d032bae3824eb940ec
Forken <samucl@gmail.com>
2020-11-15 00:36:13 +0100

    main

--- menuprincipal.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 .*-
-
-import os
-import time
-
-def menu():
-    os.system('clear')
-    os.system('sudo python tiempo.py')
--- tiempo.py
+++ tiempo.py
@@ -5,6 +5,5 @@ import os
 import time
 import requests
 
-
 tiempo = requests.get('https://wttr.in/San_Fernando,Spain?format="%l:+%c+%t+%m+Viento+%w"\n')
 print('El tiempo que hace hoy es...'+tiempo.text)
